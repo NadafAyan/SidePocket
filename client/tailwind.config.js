bfcd2ecfaaba20292f823034aecc63cbc8aab5ef
Ayan Nadaf <nadafayan95@gmail.com>
2025-03-27 19:40:18 +0530

    minor changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/Components/*.js",
-    "./src/Components/*.jsx",
+  content: ["./src/components/*.js",
+    "./src/components/*.jsx",
   ],
   theme: {
     extend: {},
