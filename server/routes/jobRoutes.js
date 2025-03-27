69ab7e7395dcf14ae0279f62bb3b91734455b2e3
Ayan Nadaf <nadafayan95@gmail.com>
2025-03-27 21:53:54 +0530

    Update jobRoutes.js

--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Job = require('../models/Job');
+const Job = require('../Models/Job');
 
 
 router.post('/post-job', async (req, res) => {
